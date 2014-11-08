72ab42d8e01df6b1d0a3ad9240d1168c773a10a4
Brandon Evans <admin@brandonevans.org>
2014-11-08 13:11:46 -0600

    Added connection error message.

--- index.php
+++ index.php
@@ -6,4 +6,10 @@ $MYSQLI = new mysqli(
 	getenv('FRIENDSWIPE_HOST'), getenv('FRIENDSWIPE_USER'),
 	getenv('FRIENDSWIPE_PASS'), getenv('FRIENDSWIPE_DB')
 );
+
+if (mysqli_connect_errno())
+{
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
 ?>
